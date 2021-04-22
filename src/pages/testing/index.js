c2ca4d57c773d9561458aeb9ad23dc34c343df74
Gustavo <carneirinho@gmail.com>
2021-04-22 14:17:22 -0300

    add basic redux / feat modal temos

--- src/pages/testing/index.js
+++ src/pages/testing/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { Container, Col } from 'react-bootstrap'
+import { useDispatch } from 'react-redux';
+import { Container, Col, Row } from 'react-bootstrap'
 import { Timeline } from '../../components'
 import { BackgroundFake } from '../../assets'
 
+
 const TestingPage = () => {
+  const dispatch = useDispatch();
+
+  const showHideTerms = (show) => {
+    dispatch({ type: 'UPDATE_TERMS', payload: true });
+  }
   return (
     <Container fluid className="p-0 content-wrapper position-relative testing">
+      <Row className="m-4">
+        <Col>
+        <div className="btn btn-success" onClick={() => showHideTerms(true) }>Open Modal</div>
+        </Col>
+      </Row>
       <Col className="col-12 timeline" style={{backgroundImage: `url(${BackgroundFake})`}}>
         <Timeline />
       </Col>
