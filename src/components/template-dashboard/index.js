431770de7e06ea63739164b59f5f66d9f38da629
alan1982br <alan1982br@gmail.com>
2021-05-06 17:06:56 -0300

    alte: fetch api add vars user hotspot

--- src/components/template-dashboard/index.js
+++ src/components/template-dashboard/index.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useEffect } from "react"
 import { Container, Row, Col } from 'react-bootstrap'
 import { useResize } from '../../hooks'
 import { ImageLoginHome } from '../../assets'
 import { Logo } from '..'
+import { useAuth } from "../../contexts/AuthContext"
 import './style.scss'
+ 
 
 window.addEventListener('message', function(e) {
-  console.log(e.origin); // outputs "http://www.example.com/"
+
+ let uid = localStorage.getItem('@Twitter:uid');
+
+  console.log(e.origin); // outputs "http://"
   console.log("receive from layer 3dvista" , e.data); // outputs "works!"
 
-  fetch('https://us-central1-casatwitter-815ac.cloudfunctions.net/api/messages?uid=OaevJixW4BZKO21i56w1JHT2W582&hotspot_id='+e.data.hotspot_id)
+  fetch('https://us-central1-casatwitter-815ac.cloudfunctions.net/api/messages?uid='+uid+'&hotspot_id='+e.data.hotspot_id+'&hotspot_name='+e.data.hotspot_name+'&hotspot_title='+e.data.hotspot_title+'&id_room='+e.data.id_room+'&room_name='+e.data.room_name)
   .then(response => response.json())
   .then(data => console.log(data));
 
 
 }, false);
 
-
-
 const TemplateDashboard = ({ component: Component, ...rest }) => {
 
   const { isMobile } = useResize();
+  const { currentUser  } = useAuth();
+
+  useEffect(() => {
+  
+
+  }, [])
 
   return (
     <Container {...rest} fluid className="template__dashboard">
