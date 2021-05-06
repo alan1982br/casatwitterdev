64ced3bc572021a86b60a8ccd2ca0ab74b9b502d
alan1982br <alan1982br@gmail.com>
2021-05-06 14:50:00 -0300

    feat: add fetch api firebase add action hotspot / update timeline data json

--- src/components/template-dashboard/index.js
+++ src/components/template-dashboard/index.js
@@ -8,6 +8,12 @@ import './style.scss'
 window.addEventListener('message', function(e) {
   console.log(e.origin); // outputs "http://www.example.com/"
   console.log("receive from layer 3dvista" , e.data); // outputs "works!"
+
+  fetch('https://us-central1-casatwitter-815ac.cloudfunctions.net/api/messages?uid=OaevJixW4BZKO21i56w1JHT2W582&hotspot_id='+e.data.hotspot_id)
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+
 }, false);
 
 
